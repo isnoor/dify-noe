d3844ad11e9856d8e4b68935c9d92d64a2307501
isnoor <isnoor.laksana@gmail.com>
2024-08-27 21:54:44 +0700

    change footer

--- web/app/(commonLayout)/apps/page.tsx
+++ web/app/(commonLayout)/apps/page.tsx
@@ -11,11 +11,7 @@ const AppList = async () => {
     <div className='relative flex flex-col overflow-y-auto bg-gray-100 shrink-0 h-0 grow'>
       <Apps />
       <footer className='px-12 py-6 grow-0 shrink-0'>
-        <h3 className='text-xl font-semibold leading-tight text-gradient'>{t('join')}</h3>
-        <p className='mt-1 text-sm font-normal leading-tight text-gray-700'>{t('communityIntro')}</p>
-        <div className='flex items-center gap-2 mt-3'>
-          <a className={style.socialMediaLink} target='_blank' rel='noopener noreferrer' href='https://discord.gg/FngNHpbcY7'><span className={classNames(style.socialMediaIcon, style.discordIcon)} /></a>
-        </div>
+        © {new Date().getFullYear()} Haufe-group.
       </footer>
     </div >
   )
